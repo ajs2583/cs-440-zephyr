6e7b5a68c4efa38e1f966aa330309e4dfa2553f2
Andrew Sliva <148899989+ajs2583@users.noreply.github.com>
2025-02-12 18:09:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-from website import create_app, db
-import logging
-from flask import Flask
+from website import create_app
 
 app = create_app()
-
-# Setup logging
-if __name__ != "__main__":
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
 
 if __name__ == "__main__":
     app.run(debug=True)
