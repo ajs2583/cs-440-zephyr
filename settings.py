602305fc0aedb4c05fcb831bb091c194bafcb370
Andrew Sliva <148899989+ajs2583@users.noreply.github.com>
2025-02-13 13:52:46 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,8 +13,11 @@ class Config:
     SQLITE_DATABASE_URL = getenv("SQLITE_DATABASE_URL")  # Local SQLite
     POSTGRES_DATABASE_URL = getenv("POSTGRES_DATABASE_URL")  # Render PostgreSQL
     
-    # Pull Boolean for use of local or server testing from .env
-    USE_LOCAL_DB = getenv("USE_LOCAL_DB", "False").strip().lower() in ("true", "1", "t")
+    '''
+    False = Server testing
+    True = Local Testing
+    '''
+    USE_LOCAL_DB = False
     
     # If true use local 
     if USE_LOCAL_DB:
