7bf64497f970408655535a985426bbd6eddaaf42
Andrew <ajs2583@nau.edu>
2025-02-23 20:48:33 -0700

    fixed main enviroment variable

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from website import create_app
+import os 
+
+DEBUG_OPTION = os.environ("DEBUG_OPTION")
 
 # Create app 
 app = create_app()
 
 # If main starts, run the app with debugger on
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=DEBUG_OPTION)
